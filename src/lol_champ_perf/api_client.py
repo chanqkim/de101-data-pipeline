dba5305fed32f96ac9ae33f4d41862947a6d0d0c
ChanKyu Kim <chk.kim87@gmail.com>
2025-11-26 16:18:28 +0900

    fix formatting and return champion_name_list to run crawling job in parallel

--- src/lol_champ_perf/api_client.py
+++ src/lol_champ_perf/api_client.py
@@ -1,8 +1,11 @@
-import requests
-import pandas as pd 
 import logging
-from src.common.file_handler import save_df_to_parquet
+
+import pandas as pd
+import requests
+
 from src.common.config import LOL_CHAMP_PERF_FILE_DIR
+from src.common.file_handler import save_df_to_parquet
+
 logger = logging.getLogger()
 
 
@@ -25,30 +28,27 @@ def fetch_json_from_url(url: str):
         logger.error(f"Failed to fetch JSON from {url}: {e}")
         return None
 
+
 def get_lol_champ_data():
-    
     versions_url = "https://ddragon.leagueoflegends.com/api/versions.json"
-    
+
     # get version_lists:  ['15.23.1', '15.22.1', ...]
     versions_list = fetch_json_from_url(versions_url)
-    
-    
+
     # first item is the recent version
-    latest_version = versions_list[0]  
-    logger.info(f'Retrived lol latest version: {latest_version}')
-    
+    latest_version = versions_list[0]
+    logger.info(f"Retrived lol latest version: {latest_version}")
 
     recent_champions_url = f"http://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/champion.json"
     champions_data = fetch_json_from_url(recent_champions_url)
 
     # create dataframe from champions data
-    champ_df = pd.DataFrame(champions_data['data']).T
-    champ_df.index = champ_df['name']
-    champ_df = champ_df.drop(columns=['name'])
+    champ_df = pd.DataFrame(champions_data["data"]).T
 
     # saving file to parquet
-    file_name = f"{latest_version.replace('.','_')}_lol_champion.parquet"
-    save_df_to_parquet(champ_df, path = LOL_CHAMP_PERF_FILE_DIR, file_name = file_name)
+    file_name = f"{latest_version.replace('.', '_')}_lol_champion.parquet"
+    save_df_to_parquet(champ_df, path=LOL_CHAMP_PERF_FILE_DIR, file_name=file_name)
     logger.info(f"champion file saved! path: {LOL_CHAMP_PERF_FILE_DIR}/{file_name}")
 
-get_lol_champ_data()
+    # return champion ids list
+    return champ_df["id"].values.tolist()
