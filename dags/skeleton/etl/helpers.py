0e454fd0e7eac51ec099a270dbeb07a20f145a78
data_engineering_101 <chk.kim87@gmail.com>
2026-01-06 14:57:20 +0900

    Delete dags directory for directory refactoring

--- dags/skeleton/etl/helpers.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-
-def hello():
-    print('Hello Airflow')
-
-with DAG(dag_id='example_dag', start_date=datetime(2025,1,1), schedule_interval='@daily') as dag:
-    task = PythonOperator(task_id='hello_task', python_callable=hello)
